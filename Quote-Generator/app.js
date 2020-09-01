44acd6cbc0944c8cdd89cb56ae3fa0197bbde026
Uday Kumar <udaykumaruking@gmail.com>
2020-09-01 17:21:56 +0530

    commit

---
+++ Quote-Generator/app.js
@@ -0,0 +1,23 @@
+$(window).keypress(function (e) {
+  if (e.which === 32) {
+    $('#quote').addClass('reset');
+    $('#quote').removeClass('executed');
+    $('#writer').toggleClass('fade');
+    setTimeout(function () {
+      $.ajax({
+        crossOrigin: true,
+        url:
+          'https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand&filter[posts_per_page]=1&_jsonp=mycallback',
+        dataType: 'jsonp',
+      });
+    }, 1000);
+  }
+});
+function mycallback(json) {
+  var quote = json[0];
+  $('#quote').html(quote.content);
+  $('#writer').html(quote.title);
+  $('#quote').addClass('executed');
+  $('#quote').removeClass('reset');
+  $('#writer').toggleClass('fade');
+}
