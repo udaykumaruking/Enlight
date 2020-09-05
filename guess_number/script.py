bec7d04ab1a567dff9a6b308c41431a9da2765b1
Uday Kumar <udaykumaruking@gmail.com>
2020-09-05 19:43:20 +0530

    enhancements and rectification

--- guess_number/script.py
+++ guess_number/script.py
@@ -1,21 +1,24 @@
 import random
 numberOfGuesses = 0
-number = random.randInt(1,50)
+number = random.randint(1,50)
 name = input("Hello! What is your name?")
 print(name + ", I am thinking of a whole number between 1 and 50. Can you guess what it is?")
 
 while numberOfGuesses < 8:
-    guess = input(int("Take a guess"))
+    guess = int(input("Take a guess"))
     numberOfGuesses = numberOfGuesses + 1
     guessesLeft = 8 - numberOfGuesses
 
-    if guess < number:
+    if guessesLeft == 0:
+            print("sorry, the number i was thinking is " + str(number) + ": )")
+    elif guess < number:
         guessesLeft = str(guessesLeft)
-        print("your guess is too low! You have" + guessesLeft + "guesses left")
-    if guess > number:
+        print("your guess is too low! You have " + guessesLeft + " guesses left")
+    elif guess > number:
         guessesLeft = str(guessesLeft)
-        print("your guess is too high! You have" + guessesLeft + "guesses left")
-    if guess == number:
-        print("good Job! You guessed the number in " + str(numberOfGuesses) + "tries :)")
-    if guess!=number:
-        print("sorry, the number i was thinking is" + str(number) + ":)")
+        print("your guess is too high! You have " + guessesLeft + " guesses left")
+        
+    elif guess == number:
+        print("good Job! You guessed the number in " + str(numberOfGuesses) + " tries :)")
+        break
+    
